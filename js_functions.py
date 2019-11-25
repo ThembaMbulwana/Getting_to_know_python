940f24efd2453ebb520b6f763e4991c50718ade9
ThembaMbulwana <thembambulwana@outlook.com>
2019-11-25 08:59:35 +0200

    Updated

--- js_functions.py
+++ js_functions.py
@@ -3,25 +3,30 @@ def list_all_js_function_names(path_to_js_file):
     path_to_js_file is a path to a file on your hard drive
     This function will read the entire input file and then return a list of js function names as strings
     """
-    with open(path_to_js_file) as f:
-        counter = 0
-        names = []
-        start = []
-        for each_line in f.readlines():
-            counter +=1
+    func_names = []
+    start_row_list =[]
+    end_row_list = []
+    
+    with open(path_to_js_file) as f:  
+        for i, each_line in enumerate (f.readlines()):
             if "function" in each_line:
-                #print(counter)
-                func = each_line.split(" ")
-                #print(func)
-                start.append(counter)
-                if "function" in func[2]:
-                    names.append(func[0])
-                if func[0] == "function":
-                    names.append(func[1].split("()")[0])
+                if "function" not in each_line.split(" ")[0] :
+                    func_names.append(each_line.split(" ")[0])
+                    start_row_list.append(i + 1)
+                else:
+                    func_names.append(each_line.split("()")[0]
+                           .split("(")[0]
+                           .split("function")[-1]
+                           .split(" ")[1])
+                    start_row_list.append(i + 1)
+                    
+            if i > 0: 
+                if each_line.split("\n")[0] == "" and each_line.split("\n")[1] == "":
+                    end_row_list.append(i)
+                    
+                if each_line.split("\n")[0] == "\t\t\t\t}" and each_line.split("\n")[1] == "":
+                    end_row_list.append(i)
+                         
+    return [{"name": v, "start_row": start_row_list[i], "end_row": end_row_list[i+1]} for i, v in enumerate(func_names) ]
 
-        print(start)
-        return names
-
-print(list_all_js_function_names("notes.txt"))
-
-
+print(list_all_js_function_names("notes.js"))
